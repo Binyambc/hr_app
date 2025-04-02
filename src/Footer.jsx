588867f6cbd87a8872fa503d0bae6214cb6d67e7
s2500141 <s2500141@edu.bc.fi>
2025-04-02 18:42:13 +0300

    Add new Components to app
    Included person descriptions to the app

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = (props) => {
+    
+    return (
+        <div class='footer'>
+            <p>{props.copyRight}</p>
+        </div>
+    );
+    };
+    
+    export default Footer;
