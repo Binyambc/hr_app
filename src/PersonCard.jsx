042bb49ebbdda3125b78840dfa7ba829dde6cce8
s2500141 <s2500141@edu.bc.fi>
2025-04-24 07:08:00 +0300

    Update step-2

---
+++ src/PersonCard.jsx
@@ -0,0 +1,40 @@
+const PersonCard = (props) => {
+
+    const calculateYearsWorked = (startDate) => {
+        const start = new Date(startDate);
+        const now = new Date();
+        const msInYears = 1000 * 60 * 60 * 24 * 365.25;
+        return (now - start) / msInYears;
+    };
+
+    const yearsWorked = calculateYearsWorked(props.startDate);
+    const fullYears = Math.floor(yearsWorked);
+    let reminder = "";
+
+    if (fullYears > 0 && fullYears % 5 === 0 && yearsWorked - fullYears < 0.1) {
+        reminder = "🎉 Schedule a recognition meeting.";
+    }
+        else if (yearsWorked < 0.5) {
+            reminder = "🔔 Schedule a probation review.";
+        }
+    
+
+    return (
+    <div className='person_container'>
+        <p>ID: {props.id}</p>
+        <p>Name: {props.firstNameLastName}</p>
+        <p>Tile: {props.title}</p>
+        <p>Salary: {props.salary}</p>
+        <p>Phone: {props.phone}</p>
+        <p>E-mail: {props.email}</p>
+        <p>Animal: {props.animal}</p>
+        <p>Start Date: {props.startDate}</p>
+        <p>Years Worked: {yearsWorked.toFixed(2)}</p>
+        <p>Department: {props.department}</p>
+        <p>Skills: {props.skills}</p>
+        {reminder && <p className="reminder">{reminder}</p> }
+    </div>
+    );
+};
+
+export default PersonCard;
---
+++ src/PersonList.jsx
@@ -0,0 +1,18 @@
+import {employees} from "./PersonData";
+import PersonCard from "./PersonCard";
+
+const PersonList = () => {
+    return (
+        <div className="person_list">
+        <h1>Employee list</h1>
+            <div className="person_grid">
+        {employees.map((employee) => (
+            <PersonCard key={employee.id} {...employee}/>
+        ))}
+            </div>
+        </div>
+    )
+}
+
+
+export default PersonList;
